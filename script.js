9df2413054ae53006c805b3c5c27cecbfc4046f3
Noty8udod <71027691+Noty8udod@users.noreply.github.com>
2023-08-01 06:48:47 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,54 +1,21 @@
-let mainContent;
-let guideContent;
 let clickCounter = 0;
-let themeToggleButton;
+let lightModeButton;
 
 document.addEventListener('DOMContentLoaded', () => {
-  themeToggleButton = document.getElementById('theme-toggle-button');
-  themeToggleButton.style.display = 'block'; // This line shows the button on the main page
+  lightModeButton = document.getElementById('light-mode-button');
+  lightModeButton.style.display = 'block';
 
-  mainContent = document.querySelector('.container');
-  guideContent = document.createElement('div');
-  guideContent.style.display = 'none';
-  guideContent.style.overflow = 'hidden';
-  document.body.appendChild(guideContent);
-
-  const links = document.querySelectorAll('table a');
-  links.forEach(link => {
-    link.addEventListener('click', event => {
-      event.preventDefault();
-      loadGuide(event.target.href);
-    });
-  });
-
-  themeToggleButton.addEventListener('click', toggleTheme);
+  lightModeButton.addEventListener('click', toggleMode);
 });
 
-async function loadGuide(url) {
-  const response = await fetch(url);
-  const text = await response.text();
-  guideContent.innerHTML = text;
-  guideContent.style.display = 'block';
-  mainContent.style.display = 'none';
-}
-
-function goBack() {
-  guideContent.style.display = 'none';
-  mainContent.style.display = 'block';
-}
-
-function toggleTheme() {
+function toggleMode() {
   const body = document.body;
   if (body.classList.contains('light-mode')) {
     body.classList.remove('light-mode');
-    themeToggleButton.textContent = 'Dark Mode';
-    themeToggleButton.style.backgroundColor = '#222';
-    themeToggleButton.style.color = '#ddd';
+    lightModeButton.textContent = 'Dark Mode';
   } else {
     body.classList.add('light-mode');
-    themeToggleButton.textContent = 'Light Mode';
-    themeToggleButton.style.backgroundColor = '#f9f9f9';
-    themeToggleButton.style.color = '#333';
+    lightModeButton.textContent = 'Light Mode';
   }
   clickCounter++;
   if (clickCounter >= 10) {
